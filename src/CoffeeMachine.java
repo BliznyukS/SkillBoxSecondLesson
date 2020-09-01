cd80c6b1b615e5c8fbaf8009ce0f1c9ba63f2eda
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2020-09-01 16:25:25 +0300

    Initial commit

---
+++ src/CoffeeMachine.java
@@ -0,0 +1,30 @@
+public class CoffeeMachine {
+/*
+    private int[] drinkPrices;
+    private String[] drinkNames;
+
+    public CoffeeMachine(int[] drinkPrices, String[] drinkNames){
+        this.drinkNames = ;
+        this.drinkPrices = ;
+
+    }
+
+    public void checkPrice(int moneyAmount) {
+        boolean canBuyAnything = false;
+
+        for (int i = 0; i < 4; i++) {
+            if (drinkPrices[i] <= moneyAmount) {
+                System.out.println("You could buy : " + drinkNames[i]);
+                canBuyAnything = true;
+            } else {
+                System.out.println("Not enough " + (drinkPrices[i] - moneyAmount) + "$  for " + drinkNames[i]);
+            }
+        }
+
+        if (canBuyAnything == false) {
+            System.out.println("Not enough money");
+        }
+    }
+
+ */
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        int[] drinkPrices = {150, 80, 20, 30};
+        String[] drinkNames = {"Cappuccinos", "Americano", "Water", "Milk"};
+
+        CoffeeMachine coffeeMachine = new CoffeeMachine(drinkNames, drinkPrices);
+        CoffeeMachine.checkPrice(120);
+    }
+
+         */
+        int[] drinkPrices = {150, 80, 20, 30};
+        System.out.println(drinkPrices[4]);
+
+
+    }
+}
